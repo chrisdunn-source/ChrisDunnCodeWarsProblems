5950e4c2f0356141b0c26f99dad3824efc962161
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2024-01-19 20:38:22 -0600

    Add files via upload

---
+++ CodeWarsProblems/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    /*
+    Make a simple function called greet that returns the most-famous "hello world"
+     */
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+    public static String greet() {
+        return "hello world!";
+    }
+
+}
