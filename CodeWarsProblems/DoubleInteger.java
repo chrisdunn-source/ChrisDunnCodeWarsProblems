1069788b7c69b908984dcc9a61a832748c5bd2bb
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2024-01-20 17:54:05 -0600

    Create DoubleInteger.java

---
+++ CodeWarsProblems/DoubleInteger.java
@@ -0,0 +1,11 @@
+//You need to double the integer and return it.
+
+class Java {
+    public static void main(String [] args){
+        System.out.println(doubleInteger(4));
+    }
+  public static int doubleInteger(int i) {
+    // Double the integer and return it!
+    return i * 2;
+  }
+}
