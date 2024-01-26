e57929fa55eb0574e20c64c1f3a1d3c84a1eed34
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2024-01-25 22:53:31 -0600

    Added Problem in Comment

--- CodeWarsProblems/HighLow.java
+++ CodeWarsProblems/HighLow.java
@@ -1,5 +1,13 @@
 public class HighLow {
     public static void main(String[] args) {
+        /*
+        In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+        Examples
+        highAndLow("1 2 3 4 5")  // return "5 1"
+        highAndLow("1 2 -3 4 5") // return "5 -3"
+        highAndLow("1 9 3 4 -5") // return "9 -5"
+        */
         System.out.println(highAndLow("1 2 3 4 5"));    // Output: "5 1"
         System.out.println(highAndLow("1 2 -3 4 5"));   // Output: "5 -3"
         System.out.println(highAndLow("1 9 3 4 -5"));   // Output: "9 -5"
