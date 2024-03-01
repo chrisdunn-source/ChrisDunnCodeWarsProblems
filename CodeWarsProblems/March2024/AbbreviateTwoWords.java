f729ad666695c2819b6ed7795c397452ddd7ca2b
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2024-03-01 14:20:27 -0600

    Update AbbreviateTwoWords.java

--- CodeWarsProblems/March2024/AbbreviateTwoWords.java
+++ CodeWarsProblems/March2024/AbbreviateTwoWords.java
@@ -1,5 +1,11 @@
 public class AbbreviateTwoWords {
-
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
+Sam Harris => S.H
+patrick feeney => P.F
+*/
     
   public static String abbrevName(String name) {
     String[] words = name.split(" "); // Split the name into words using space as the delimiter
