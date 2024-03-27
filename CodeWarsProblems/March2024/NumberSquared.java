c35a309a9fe1030936be85a03712d6368f0b1646
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2024-03-27 14:39:18 -0500

    Add files via upload

---
+++ CodeWarsProblems/March2024/NumberSquared.java
@@ -0,0 +1,11 @@
+package March2024;
+//Now you have to write a function that takes an argument and returns the square of it.
+public class NumberSquared {
+    public static void main(String[] args) {
+        System.out.println(square(12));
+    }
+    public static int square(int n){
+        return n * n;
+
+    }
+}
